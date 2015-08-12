5f1e6f345dcff84c149d3b9e77969749217244e4
dbtucker <dbtucker@cal.berkeley.edu>
2015-08-12 15:21:50 -0700

    Refactored to support more complex installs.

--- azure-wrapper.sh
+++ azure-wrapper.sh
@@ -9,22 +9,28 @@
 #	when chaining them together here.
 #
 # The key to the deployment is generating the hosts file to be used
-# for cluster formation (since Azure does not yet support DNS lookup
-# of hostnames assigned during resource creation.   We assume that
-# the hosts are all of the form <base><n>, where <n> varies from 0 to
-# cluster_size - 1.   The IP addresses are of the form <prefix><m>,
-# were <m> is the index of the host plus the <first_ip> parameter.
+# for cluster formation.  We assume that the hosts are all of the 
+# form <base><n>, where <n> varies from 0 to cluster_size - 1.   
+#
 #
 #
 # USAGE :
-#	$0 [ <basename> ] [ <cluster_size> ] [ <IP_subnet_prefix> ] [ <first_ip> ]
+#	$0 [ <basename> ] [ <size> ] [ <edition> ] [ <mapr_version> ]
+#
+#		<edition> defaults to M3
+#		<mapr_version> defaults to 5.0.0
 #
 # EXAMPLE :
-#	$0 testnode 4 10.0.0. 10
+#	$0 testnode 4 M5
 #
 #		The effect would be a 4-node cluster with testnode0, testnode1, 
-#		testnode2, and testnode3 (at 10.10.10.[10-13]).
+#		testnode2, and testnode3 ... licensed for M5
+#
+# TBD
+#	Probably don't need the <basename> property, since we can extract it
+#	from our own hostname ... but we'll keep it this way for now.
 #	
+
 
 THIS=`readlink -f $0`
 BINDIR=`dirname $THIS`
@@ -32,11 +38,19 @@ BINDIR=`dirname $THIS`
 HOSTNAME=`hostname`
 CLUSTER_HOSTNAME_BASE="${HOSTNAME%node*}node"
 
-sh $BINDIR/gen-cluster-hosts.sh ${1:-$CLUSTER_HOSTNAME_BASE} ${2:-3} ${3:-} ${4:-}
+sh $BINDIR/gen-cluster-hosts.sh ${1:-$CLUSTER_HOSTNAME_BASE} ${2:-3} 
 
 sh $BINDIR/prepare-disks.sh
 
+# These should be passed in via metadata
+export MAPR_PASSWD=MapRAZ
+export MAPR_VERSION=${4:-5.0.0} 
 sh $BINDIR/prepare-node.sh
+
+echo "${3:-M3} > /tmp/maprlicensetype	# must match deploy-mapr-ami.sh
+CFG_DIR=/home/mapr/cfg					# must match deploy-mapr-ami.sh
+cp -p $BINDIR/*.lst $CFG_DIR
+chown -R mapr:mapr $CFG_DIR
 
 sh $BINDIR/deploy-mapr-ami.sh
 
