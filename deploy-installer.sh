1cf6c736858e5d3c0712cff9f9ca3e8e72c226c1
dbtucker <dbtucker@cal.berkeley.edu>
2015-12-30 15:45:19 -0800

    Minor fixes to handle JDK

--- deploy-installer.sh
+++ deploy-installer.sh
@@ -6,12 +6,15 @@ BINDIR=`dirname $THIS`
 LOG=/tmp/deploy-installer.log
 
 INSTALLER_SETUP_URI=http://package.mapr.com/releases/installer/mapr-setup.sh
+INSTALLER_HOME=/opt/mapr/installer
 
 # The MapR Admin User defaults to "mapr" in the mapr-setup.sh script
 # We'll set a different password (tied to the instance-id by default
 murl_top=http://instance-data/latest/meta-data
 INSTANCE_ID=$(curl -f $murl_top/instance-id)
 MAPR_USER=${MAPR_USER:-mapr}
+MAPR_GROUP=`id -gn ${MAPR_USER}`
+MAPR_GROUP=${MAPR_GROUP:-mapr}
 # MAPR_PASSWD=${MAPR_PASSWD:-${INSTANCE_ID}}
 MAPR_PASSWD=${MAPR_PASSWD:-MapRAZ}
 
@@ -34,6 +37,39 @@ function reset_epel() {
 	yum-config-manager -enable epel		    
 }
 
+# Lots of AWS AMI's come with just the openjdk JRE installed;
+# Try to make sure we have the complete JDK (when we can)
+function completeJDK() {
+	if which dpkg &> /dev/null ; then
+		for jver in 7 8 ; do
+			dpkg -l openjdk-${jver}-jre &> /dev/null
+			[ $? -eq 0 ] && apt-get install -y openjdk-${jver}-jdk
+		done
+	elif which rpm &> /dev/null ; then
+		for jver in 1.7.0 1.8.0 ; do
+			rpm -qi java-${jver}-openjdk &> /dev/null
+			[ $? -eq 0 ] && yum install -y java-${jver}-openjdk-devel
+		done
+	fi
+}
+
+function apply_nfs_mount_patch() {
+	PLAYBOOKS_HOME=${INSTALLER_HOME}/ansible/playbooks
+
+	cp $BINDIR/mount_local_fs.pl ${PLAYBOOKS_HOME}/files
+	[ $? -ne 0 ] && return
+
+	chmod a+r ${PLAYBOOKS_HOME}/files/mount_local_fs.pl
+	chown --reference=${PLAYBOOKS_HOME} ${PLAYBOOKS_HOME}/files/mount_local_fs.pl
+
+	cat << IP_EOF >> $PLAYBOOKS_HOME/install_packages.yml
+
+  - copy: src=files/mount_local_fs.pl  dest="{{ mapr_home }}/bin/mount_local_fs.pl" backup=yes owner=root group=root mode=0755
+    when: pkgs.find('mapr-nfs') != -1
+
+IP_EOF
+}
+
 function main() {
 	echo "$0 script started at "`date`   | tee -a $LOG
 	echo "    with args: $@"             | tee -a $LOG
@@ -53,6 +89,7 @@ function main() {
 	fi
 
 #	reset_epel
+	completeJDK
 
 		# We may need to disable the requiretty constraint on sudo
 #	sed -i 's/ requiretty/ !requiretty/' /etc/sudoers
@@ -68,7 +105,7 @@ function main() {
 
 		# Make sure we have the "requests" package enabled 
 		# (necessary for python script to drive the installer)
-	/opt/mapr/installer/build/python/bin/pip install requests
+	${INSTALLER_HOME}/build/python/bin/pip install requests
 	
 		# And wait for the service to come to life (at least 2 minutes)
 	SWAIT=120
@@ -79,6 +116,12 @@ function main() {
 		SWAIT=$[SWAIT - $STIME]
 		curl -f -k -u mapr:$MAPR_PASSWORD https://localhost:9443 &> /dev/null
 	done
+
+		# If we need to debug the installer
+#	sed -i "s/\"debug\": false/\"debug\": true/" ${INSTALLER_HOME}/data/properties.json
+#	service mapr-installer reload
+
+	return 0
 
 # Once the installer has been successfully installed, you can 
 # use the python script to talk with it
@@ -93,11 +136,12 @@ function main() {
 
 }
 
-set -x
-
 main $@
 exitCode=$?
 
-set +x
+# Long term, we only need to do this for 4.1.0 and 5.0.0
+#	We can gate it by removing the fixed mount_local_fs.pl script
+#	(see installer-wrapper.sh).
+apply_nfs_mount_patch
 
 exit $exitCode
